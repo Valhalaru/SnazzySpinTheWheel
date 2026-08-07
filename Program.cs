71488bd51286f5f8e3e57de1ea7655a0b318b53c
milamber <milam@VALHALARU>
2026-08-07 16:02:06 -0700

    Commit the BUGGY Version

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
 ﻿Console.WriteLine("Hello, World!");
+<<<<<<< HEAD
 Console.Write("lets push");
+alsdaslkdhlkasdhalskdhl error
+    adhaslkdhksajh
+=======
+>>>>>>> parent of 6f9dadc (added 1 line #Program.cs)
