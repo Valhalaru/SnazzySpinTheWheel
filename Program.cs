f588894071a75ae716de7df37c3a48994f89ed2b
milamber <milam@VALHALARU>
2026-08-07 16:11:21 -0700

    2 lines of awesome

--- Program.cs
+++ Program.cs
@@ -1,7 +1,2 @@
 ﻿Console.WriteLine("Hello, World!");
-<<<<<<< HEAD
 Console.Write("lets push");
-alsdaslkdhlkasdhalskdhl error
-    adhaslkdhksajh
-=======
->>>>>>> parent of 6f9dadc (added 1 line #Program.cs)
